91f4a1aa3852948b1e7190014fbf836811ac9f28
ChenYin-Yu <Chenyinyu7526@gmail.com>
2023-02-12 15:37:37 -0800

    Updates

---
+++ precache-manifest.02682916b92e8d2e34cf92ed310a701e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e42fe8e808d92b76192",
+    "url": "/helloitsChenYin/static/css/main.ddfe8b91.chunk.css"
+  },
+  {
+    "revision": "2e42fe8e808d92b76192",
+    "url": "/helloitsChenYin/static/js/main.2e42fe8e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/helloitsChenYin/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/helloitsChenYin/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "95c034b23703ac5820b5",
+    "url": "/helloitsChenYin/static/js/runtime~main.95c034b2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/helloitsChenYin/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e346e295389e6a2a97577a7e89bcf73a",
+    "url": "/helloitsChenYin/index.html"
+  }
+];
